607b2ac70c33a6f25e46a8bbbc53da2b1927ba70
Harit Aukkahat <42645488+RiceeBoye@users.noreply.github.com>
2019-05-29 14:00:49 +0200

    Add files via upload

---
+++ WU1-Haak/Assets/javascripts/java.js
@@ -0,0 +1,7 @@
+function warning1() {
+    alert ("You are now going to the starting page");
+}
+
+function submitted(){
+    alert ("Thank your effort ")
+}
---
+++ WU1-Haak/java.js
@@ -0,0 +1,7 @@
+function warning1() {
+    alert ("You are now going to the starting page");
+}
+
+function submitted(){
+    alert ("Thanks for your effort ");
+}
